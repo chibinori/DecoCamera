a80ad6a94766fe57481996aa86a8971d49f96070
Noriaki Sakai <noriaki.sakai.develop@gmail.com>
2015-12-19 10:37:13 +0900

    Initial Commit

---
+++ DecoCamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DecoCamera
+//
+//  Created by 酒井紀明 on 2015/12/19.
+//  Copyright © 2015年 noriaki.sakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
